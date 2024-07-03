e545e879cbfa24e6ebef01c7414817c88f9ec2f9
mehdaoui-fatima <mehdaoui.fa@gmail.com>
2024-07-03 11:02:10 +0100

    init container mariadb

---
+++ srcs/requirements/mariadb/conf/startdb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# if [ ! -d "/var/lib/mysql/$DB_NAME" ];
+# then
+#     mysql_install_db
+
+#     mariadb service start
+
+#     sleep 2
+    
+#     echo "teest"
+#     # create user
+# fi
+
+#     echo "teest 2"
+
+
+mysqld_safe
